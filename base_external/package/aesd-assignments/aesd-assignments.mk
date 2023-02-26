cabcdbd443c37b01028d112c5abf96c39e294699
Pascal Jäger <pascal.jaeger@leimstift.de>
2023-02-26 15:49:12 +0100

    complete assignment-5-part2
    Signed-off-by: Pascal Jäger <pascal.jaeger@leimstift.de>

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '601b453'
+AESD_ASSIGNMENTS_VERSION = 'ce0ecf33e9345f4088205dbc3d7849d6bee84b5c'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -15,7 +15,12 @@ AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	CROSS_COMPILE="/__w/assignment-4-Schievel1/assignment-4-Schievel1/buildroot/output/host/bin/aarch64-buildroot-linux-uclibc-"  $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app default
+	CROSS_COMPILE="aarch64-buildroot-linux-uclibc-" $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app default
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server
+#	CROSS_COMPILE="/home/pascal/dev/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-" $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app default
+#	CROSS_COMPILE="/home/pascal/dev/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-" $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server
+#	CROSS_COMPILE="/__w/assignment-5-Schievel1/assignment-5-Schievel1/buildroot/output/host/bin/aarch64-buildroot-linux-uclibc-" $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app default
+#	CROSS_COMPILE="/__w/assignment-5-Schievel1/assignment-5-Schievel1/buildroot/output/host/bin/aarch64-buildroot-linux-uclibc-" $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
@@ -26,6 +31,8 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
 $(eval $(generic-package))
